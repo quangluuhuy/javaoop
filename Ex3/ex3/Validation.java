4cfaa2b23848ac27406a988598129f885e509d7d
quangluuhuy <112995974+quangluuhuy@users.noreply.github.com>
2022-12-16 16:04:51 +0700

    Update Validation.java

--- Ex3/ex3/Validation.java
+++ Ex3/ex3/Validation.java
@@ -4,7 +4,10 @@
  */
 package ex3;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -13,18 +16,16 @@ import java.util.Scanner;
  */
 public class Validation {
 
-    static boolean checkChangeInfo(ArrayList<Student> id, String name, String stduentId, int age, String classs, String school) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+   
 
     
-    private final Scanner sc = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public String checkInputString(String mess) {
 
         while (true) {
             System.out.print(mess);
-            String result = sc.nextLine().trim();
+            String result = scanner.nextLine().trim();
             if (result.isEmpty()) {
                 System.err.println("Not empty.");
 
@@ -36,7 +37,7 @@ public class Validation {
     public int checkInt(String mess,int min, int max) {
         while (true) {
             System.out.print(mess);
-            String raw = sc.nextLine().trim();
+            String raw = scanner.nextLine().trim();
             if (!raw.isEmpty()) {
                 try {
                     int choice = Integer.parseInt(raw);
@@ -54,7 +55,31 @@ public class Validation {
             }
         }
     }
-     
+     public String checkDate(String mess) {
+       
+
+        while (true) {
+            System.out.print(mess);
+            String raw = scanner.nextLine().trim();
+            if (!raw.isEmpty()) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                try {
+                    Date date = sdf.parse(raw);
+                    String result_str = sdf.format(date);
+                    if (result_str.equals(raw)) {
+                        return result_str;
+                    } else {
+                        System.err.println("Please enter a right date! ");
+                    }
+                } catch (ParseException e) {
+                    System.err.println("Date must in format dd-MM-yyyy! ");
+                }
+            } else {
+                System.err.println("Date can not empty! ");
+            }
+        }
+        
+    }
     public Student isIdExists(ArrayList<Student> students, int id) {
         for (Student student : students) {
             if (student.getId() == id) {
